60c2c459b0155ea5edab9e1e3aef3b1a4d3b5378
Josè Angel <ja.velazquez.perez@gmail.com>
2022-12-14 19:23:56 -0600

    nuevo microservicio 2da parte

---
+++ nucleo-dash-msvc-followers/README.md
@@ -0,0 +1,1 @@
+# nucleo-dash-msvc-followers
---
+++ nucleo-dash-msvc-followers/src/main/java/com/nucleodash/followers/NucleoDashMsvcFollowersApplication.java
@@ -0,0 +1,13 @@
+package com.nucleodash.followers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleoDashMsvcFollowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.nucleodash.followers.NucleoDashMsvcFollowersApplication.class, args);
+	}
+
+}
