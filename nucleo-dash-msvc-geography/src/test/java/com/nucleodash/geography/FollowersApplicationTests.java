ec1aeaed464ba1f7f2b238b21fa273c6be534095
juan roberto <juanros13@gmail.com>
2022-12-31 16:55:00 -0600

    geography module

---
+++ nucleo-dash-msvc-geography/src/test/java/com/nucleodash/geography/FollowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleodash.followers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
